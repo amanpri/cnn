853b2aa2be42ea0f8d296346c1df8f77a7670e64
kishu <37251055+amanpri@users.noreply.github.com>
2020-10-29 17:14:17 +0530

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-# loss
+# loss            #output , y
 def cross_entropy(inputs, labels):
 
     out_num = labels.shape
-    p = np.sum(labels.reshape(1,out_num)*inputs)
+    #p = np.sum(labels.reshape(1,out_num)*inputs)
+    p=abs(labels-inputs)
     loss = -np.log(p)
     return loss
